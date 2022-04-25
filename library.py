74782a0b894f009438c75d3dde3bc970527296df
Muhammad-saad-2000 <mohamedmiogh2@gmail.com>
2022-04-25 23:13:11 +0200

    Adding the theortical aspect. Some modularity modifications.

---
+++ library.py
@@ -0,0 +1,62 @@
+# %%
+import numpy as np
+from math import erfc, sqrt
+
+# %% [markdown]
+# # Transmitter
+# Involves a binary source generating random 0s and 1s and a pulse shape filter that fits pulses on the bitstream.
+
+# %%
+def BinarySource(n):
+	return np.random.randint(0,2,n)
+
+# %%
+def binarycode_to_signal(bitstream, step):
+    T = 1  # assume the pulse period is 1
+    A = 1  # assume the amplitude is 1
+    pulse = np.ones(int(T/step))
+    pulse = pulse*A
+    signal = np.zeros(len(bitstream)*len(pulse))
+    # Polar nonreturn to zero
+    for i in range(len(bitstream)):
+        if bitstream[i] == 1:
+            signal[i*len(pulse):(i+1)*len(pulse)] = 1*pulse    # take the shape of pulse along the symbol's interval
+        else:
+            signal[i*len(pulse):(i+1)*len(pulse)] = -1*pulse
+    return signal
+
+
+# %% [markdown]
+# # Channel
+# Only adds additive white Gaussian noise on the signal
+
+# %%
+def AWGN(n, sigma):
+	return np.random.normal(0,sigma,n)
+
+# %% [markdown]
+# # Receiver
+# Receive signal from the channel then pass it by receive felter, sampler and decision maker.
+
+# %%
+def receive_filter(signal_noise, filter_num, step):
+   filter_num-=1
+   filters = [np.ones(int(1/step)), np.ones(1), np.sqrt(3)*np.arange(0, 1, step)]
+   filter = filters[filter_num]
+   filter = np.concatenate((filter, np.zeros(int(1/step)-len(filter))))
+   signal_noise_filter=np.convolve(signal_noise, filter)
+   return signal_noise_filter
+
+
+
+# %%
+def sampler(sampling_period, signal_noise_filtered, n=10):
+   samples = np.zeros(n)
+   for i in range(len(samples)):
+      samples[i] = signal_noise_filtered[(i+1)*sampling_period]
+   return samples
+
+# %%
+Q = lambda x : 0.5 * erfc(x/sqrt(2))
+def decision_maker(samples, λ):
+   return (samples>λ)*1
--- simulation.py
+++ simulation.py
@@ -1,35 +1,11 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from library import AWGN, BinarySource, binarycode_to_signal, receive_filter, Q, decision_maker, sampler
 # simulation parameters
-n = 100000
+n = 1000000
 step = 0.05
 t = np.arange(0, n, step)
-
-
-def AWGN(n, sigma):
-    return np.random.normal(0, sigma, n)
-
-
-def BinarySource(n):
-    return np.random.randint(0, 2, n)
-
-
-def binarycode_to_signal(bitstream, step):
-    T = 1  # assume the pulse period is 1
-    A = 1  # assume the amplitude is 1
-    pulse = np.ones(int(T/step))
-    pulse = pulse*A
-    signal = np.zeros(len(bitstream)*len(pulse))
-    # Polar nonreturn to zero
-    for i in range(len(bitstream)):
-        if bitstream[i] == 1:
-            signal[i*len(pulse):(i+1)*len(pulse)] = 1*pulse
-        else:
-            signal[i*len(pulse):(i+1)*len(pulse)] = -1*pulse
-    return signal
-
 
 # generate the binary symbols
 bitstream = BinarySource(n)
@@ -39,12 +15,6 @@ bitstream = BinarySource(n)
 signal = binarycode_to_signal(bitstream, step)
 
 
-# construct the resive filter (We have 3 filters to choose from)
-filter1 = np.ones(int(1/step))
-filter2 = np.ones(1)
-filter3 = np.sqrt(3)*np.arange(0, 1, step)
-
-
 def error_rate(sigma_noise, filter_num):
     # generate the noise
     noise = AWGN(len(signal), sigma_noise)
@@ -52,51 +22,52 @@ def error_rate(sigma_noise, filter_num):
     # add the noise to the signal
     signal_noise = signal+noise
 
-    # select the filter
-    filter = filter1 if filter_num == 1 else filter2 if filter_num == 2 else filter3
-    filter = np.concatenate((filter, np.zeros(int(1/step)-len(filter))))
-
     # apply the filter to the signal
-    signal_noise_filter = np.convolve(signal_noise, filter)
+    signal_noise_filter = receive_filter(signal_noise,filter_num, step)
 
     # sample the filtered signal
     sampling_period = int(1/step)
-    samples = np.zeros(n)
-    for i in range(len(samples)):
-        samples[i] = signal_noise_filter[(i+1)*sampling_period]
+    samples = sampler(sampling_period, signal_noise_filter,n)
 
     # decode the samples
-    reconstructed_bitstram = (samples > 0)*1
-
+    reconstructed_bitstram = decision_maker(samples, 0)
     # results
     return np.sum(bitstream != reconstructed_bitstram)/len(bitstream)
 
 
 
 # plot E/No vs error rate (first filter)
-
 x=np.arange(-10, 20, 1)
 sigma_noise=np.sqrt(2/(10**(x/10)))
 
 error_rate_1 = np.zeros(len(sigma_noise))
+theorey_error_rate_1=np.zeros(len(sigma_noise))
 for i in range(len(sigma_noise)):
     error_rate_1[i] = error_rate(sigma_noise[i], 1)
+    theorey_error_rate_1[i] = Q(1/sigma_noise[i])
 
 error_rate_2 = np.zeros(len(sigma_noise))
+theorey_error_rate_2=np.zeros(len(sigma_noise))
 for i in range(len(sigma_noise)):
     error_rate_2[i] = error_rate(sigma_noise[i], 2)
+    theorey_error_rate_2[i] = Q(1/sigma_noise[i])
 
 error_rate_3 = np.zeros(len(sigma_noise))
+theorey_error_rate_3 = np.zeros(len(sigma_noise))
 for i in range(len(sigma_noise)):
     error_rate_3[i] = error_rate(sigma_noise[i], 3)
-
+    theorey_error_rate_3[i] = Q(np.sqrt(3)/2*1/sigma_noise[i])
 
 plt.semilogy(x, error_rate_1, 'r')
 plt.semilogy(x, error_rate_2, 'g')
 plt.semilogy(x, error_rate_3, 'b')
+plt.semilogy(x, theorey_error_rate_1, 'c')
+plt.semilogy(x, theorey_error_rate_2, 'm--')
+plt.semilogy(x, theorey_error_rate_3, 'y')
 
 plt.xlabel('E/No (dB)')
 plt.ylabel('BER')
 plt.title('E/No vs BER')
-plt.legend(['First filter', 'Second filter', 'Third filter'])
-plt.savefig('./out.png')
+plt.legend(['First filter', 'Second filter', 'Third filter', 'Theorey 1', 'Theorey 2', 'Theorey 3'])
+plt.ylim([1/n*100, 1])
+plt.savefig('./BER.png')
